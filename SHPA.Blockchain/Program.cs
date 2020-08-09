9e299fd9407708e873a8c994a64949b82b978fa3
ali <akyegane@gmail.com>
2020-08-09 10:16:41 +0430

    init console project

---
+++ SHPA.Blockchain/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SHPA.Blockchain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
