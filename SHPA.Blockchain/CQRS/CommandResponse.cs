b61c415b8fdf78e31ce1f7c87a1db163e1c87489
ali <akyegane@gmail.com>
2020-09-01 12:34:16 +0430

    add mediator

---
+++ SHPA.Blockchain/CQRS/CommandResponse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SHPA.Blockchain.CQRS
+{
+    public abstract class CommandResponse : ICommandResponse
+    {
+        private readonly Guid _id;
+        private readonly Guid _commandId;
+        private readonly DateTime _time;
+
+        public CommandResponse(Guid commandId)
+        {
+            _id = Guid.NewGuid();
+            _commandId = commandId;
+            _time = DateTime.UtcNow;
+        }
+
+        public Guid GetCommandId()
+        {
+            return _commandId;
+        }
+
+        public Guid GetId()
+        {
+            return _id;
+        }
+
+        public DateTime GetTimespan()
+        {
+            return _time;
+        }
+    }
+}
---
+++ SHPA.Blockchain/CQRS/Interfaces.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SHPA.Blockchain.CQRS
+{
+    public interface ICommand<TResponse> where TResponse : ICommandResponse
+    {
+        string GetType();
+        Guid GetId();
+        DateTime GetTimespan();
+    }
+
+    public interface ICommandResponse
+    {
+        Guid GetCommandId();
+        Guid GetId();
+        DateTime GetTimespan();
+    }
+
+    public interface ICommandHandler<in TCommand, out TCommandResponse> where TCommand : ICommand<TCommandResponse>
+        where TCommandResponse : ICommandResponse
+    {
+        Task<ICommandResponse> Handle(ICommand<ICommandResponse> command, CancellationToken cancellationToken);
+    }
+}
